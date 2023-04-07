8644314ec5ac0c27eedcd315dd24b46666354023
jundaeyoung <jdy1787@naver.com>
2023-04-07 12:27:43 +0900

    get방식

---
+++ demo1/src/main/java/com/example/demo/model/PostReqDto.java
@@ -0,0 +1,21 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor   // 기본 생성자 만드는것
+@AllArgsConstructor // 모든 생성자 만드는것
+public class PostReqDto {
+	
+	private String name;
+	private String email;
+	private int age;
+	private int hobby;
+	private int phoneNumber;
+}
