1bb96d77d68c6d82391be687480997b6d31b4f24
jundaeyoung <jdy1787@naver.com>
2023-04-07 14:04:24 +0900

    POST 방식의 이해

--- demo1/src/main/java/com/example/demo/model/PostReqDto.java
+++ demo1/src/main/java/com/example/demo/model/PostReqDto.java
@@ -1,5 +1,7 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,13 +11,14 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-@NoArgsConstructor   // 기본 생성자 만드는것
+@NoArgsConstructor // 기본 생성자 만드는것
 @AllArgsConstructor // 모든 생성자 만드는것
 public class PostReqDto {
-	
+
 	private String name;
 	private String email;
 	private int age;
 	private int hobby;
+	@JsonProperty("phone_number")
 	private int phoneNumber;
 }
